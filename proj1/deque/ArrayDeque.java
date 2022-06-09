bcc2be0199fceff2d122eaaf53d004deabfe3c05
谢昊旸 <xiehaoyang@nat-wireless-guest-reg-153-50.bu.edu>
2022-06-08 20:45:05 -0400

    done with proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,37 +2,53 @@ package deque;
 
 import java.util.Iterator;
 
-public class ArrayDeque<T> implements Deque<T>{
+public class ArrayDeque<T> implements Deque<T> {
+
+    /* Deque implementation using array */
     private int capacity;
-    private int size;
     private T[] items;
     private int first;
     private int last;
+    private int size;
 
+    /** Constructor, creates an empty linked list deque*/
     public ArrayDeque() {
         capacity = 8;
+        items = (T[]) new Object[capacity];
         size = 0;
-        items = (T[])new Object[capacity];
     }
 
-    private  boolean isFull() {
+    /* Check if the deque is full */
+    public boolean isFull() {
         return size == capacity;
     }
 
-    private void resize(double factor) {
-        int newCapacity = (int)(capacity * factor);
+    /* Resize method: should be very tricky */
+    public void resize(double scalingFactor) {
+        int newCapacity = (int) scalingFactor * capacity;
+        //int newCapacity = (int) 100+ capacity;
         T[] newItems = (T[]) new Object[newCapacity];
-        System.arraycopy(items, 0, newItems, 0, size);
+
+        // Looping the number of size times
+        for (int i = 0; i < size; i++) {
+            newItems[(first + i) % newCapacity] = items[(first + i) % capacity];
+        }
+
+        last = (first + size - 1) % newCapacity;
         capacity = newCapacity;
         items = newItems;
     }
 
+    /* Should take constant time */
     @Override
-    // take constant time
     public int size() {
         return size;
     }
 
+    /** Add item at the first of deque
+     * The add method should not use any looping or recursion
+     * such operation should take constant amount of time, not depend on size
+     */
     @Override
     public void addFirst(T item) {
         if (isFull()) {
@@ -51,15 +67,17 @@ public class ArrayDeque<T> implements Deque<T>{
         size += 1;
     }
 
+    /* Add item at the last of deque */
     @Override
     public void addLast(T item) {
         if (isFull()) {
             resize(2);
         }
+
         if (isEmpty()) {
             first = 0;
             last = 0;
-        } else if(last == capacity - 1) {
+        } else if (last == capacity - 1) {
             last = 0;
         } else {
             last += 1;
@@ -68,6 +86,7 @@ public class ArrayDeque<T> implements Deque<T>{
         size += 1;
     }
 
+    /* Remove the item at the first of deque */
     @Override
     public T removeFirst() {
         if (isEmpty()) {
@@ -88,6 +107,7 @@ public class ArrayDeque<T> implements Deque<T>{
         return delItem;
     }
 
+    /* Remove the item at the last of deque */
     @Override
     public T removeLast() {
         if (isEmpty()) {
@@ -100,7 +120,7 @@ public class ArrayDeque<T> implements Deque<T>{
         items[last] = null;
         size -= 1;
 
-        if(last == 0) {
+        if (last == 0) {
             last = capacity - 1;
         } else {
             last -= 1;
@@ -108,55 +128,60 @@ public class ArrayDeque<T> implements Deque<T>{
         return delItem;
     }
 
+    /* Print all items in deque */
     @Override
     public void printDeque() {
-        int firstPointer = first;
-        int lastPointer = last;
+        int firstPos = first;
+        int lastPos = last;
 
-        while(firstPointer != lastPointer) {
-            System.out.print(items[first] + " ");
-            if (firstPointer == capacity - 1) {
-                firstPointer = 0;
+        while (firstPos != lastPos) {
+            System.out.print(items[firstPos] + " ");
+            if (firstPos == capacity - 1) {
+                firstPos = 0;
             } else {
-                firstPointer += 1;
+                firstPos += 1;
             }
         }
-        System.out.print(items[firstPointer]);
+        System.out.print(items[firstPos]);
         System.out.println();
     }
 
+    /** Get the item at the specified index
+     * Must use iteration, not recursion
+     */
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index > size) {
             return null;
         }
 
-        int pos = (first + index) % capacity;
-        return items[pos];
+        int currentPos = (first + index) % capacity;
+        return items[currentPos];
     }
 
-    public Iterator<T> iterator() {
-        return new ArrayDequeIterator();
+    private Iterator<T> iterator() {
+        return new arrayDequeIterator();
     }
 
-    private class ArrayDequeIterator implements Iterator<T> {
-        private int pos;
+    private class arrayDequeIterator implements Iterator<T> {
+        private int position;
 
-        public ArrayDequeIterator() {
-            pos = 0;
+        public arrayDequeIterator() {
+            position = 0;
         }
 
         public boolean hasNext() {
-            return pos < size;
+            return position < size;
         }
 
         public T next() {
-            T result = items[(first+pos)%capacity];
-            pos ++;
-            return result;
+            T returnItem = items[(first+position) % capacity];
+            position += 1;
+            return returnItem;
         }
     }
 
+    /* Comparison using class method */
     @Override
     public boolean equals(Object o) {
         if (o == null) {
@@ -167,21 +192,20 @@ public class ArrayDeque<T> implements Deque<T>{
             return true;
         }
 
-        ArrayDeque<T> other = (ArrayDeque<T>) o;
-        if(other.size() != this.size()) {
+        ArrayDeque<T> otherObject = (ArrayDeque<T>) o;
+        if (otherObject.size() != this.size()) {
             return false;
         }
 
-        Iterator<T> otherIterator = other.iterator();
-        Iterator<T> thisIterator = this.iterator();
-
-        while(otherIterator.hasNext() && thisIterator.hasNext()) {
-            if(thisIterator.next() != otherIterator.next()) {
+        Iterator<T> otherObjectIter = otherObject.iterator();
+        Iterator<T> thisIter = iterator();
+        while (otherObjectIter.hasNext()) {
+            if (otherObjectIter.next() != thisIter.next()) {
                 return false;
             }
         }
+
+        // All tests passed, return true
         return true;
-
     }
-
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,30 +1,43 @@
 package deque;
 
-import org.junit.Test;
-
 import java.util.Iterator;
 
-public class LinkedListDeque<T> implements Deque<T> {
-    private Node<T> sentinel;
-    private  int size;
+public class LinkedListDeque<T> implements Deque<T>{
 
+    private Node<T> sentinel;
+    private int size;
+
+    /** Deque implementation using linked list
+     * Make it circular
+     */
     private class Node<T> {
-        public T item;
-        public Node<T> prev;
+        public T data;
         public Node<T> next;
+        public Node<T> prev;
 
         public Node(T x) {
-            item = x;
+            data = x;
         }
     }
 
+    /* Constructor, creates an empty linked list deque */
     public LinkedListDeque() {
-        sentinel = new Node<T>(null);
+        sentinel = new Node<T>(null);  // Not sure for now...
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
         size = 0;
     }
 
+    /* Should take constant time */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /** Add item at the first of deque
+     * The add method should not use any looping or recursion
+     * such operation should take constant amount of time, not depend on size
+     */
     @Override
     public void addFirst(T item) {
         Node<T> newNode = new Node<T>(item);
@@ -50,7 +63,7 @@ public class LinkedListDeque<T> implements Deque<T> {
         if (isEmpty()) {
             return null;
         }
-        T first = sentinel.next.item;
+        T first = sentinel.next.data;
         sentinel.next.next.prev = sentinel;
         sentinel.next = sentinel.next.next;
         size -= 1;
@@ -62,20 +75,14 @@ public class LinkedListDeque<T> implements Deque<T> {
         if (isEmpty()) {
             return null;
         }
-        T last = sentinel.prev.item;
+        T last = sentinel.prev.data;
         sentinel.prev.prev.next = sentinel;
         sentinel.prev = sentinel.prev.prev;
         size -= 1;
         return last;
     }
 
-
-    @Override
-    // take constant time
-    public int size() {
-        return size;
-    }
-
+    /* Print all items in deque */
     @Override
     public void printDeque() {
         if (size == 0) {
@@ -83,63 +90,75 @@ public class LinkedListDeque<T> implements Deque<T> {
             return;
         }
 
-        Node<T> pointer = sentinel;
-        while (pointer.next != sentinel) {
-            System.out.print(pointer.item + " ");
-            pointer = pointer.next;
+        // currentNode points at sentinel
+        Node<T> currentNode = sentinel;
+        System.out.print("{");
+
+        // Iterate over the queue
+        while (currentNode.next.next != sentinel) {
+            currentNode = currentNode.next;
+            System.out.print(currentNode.data + " ");
         }
-        System.out.print(pointer.item + " ");
+        currentNode = currentNode.next;
+        System.out.print(currentNode.data + "}");
         System.out.println();
     }
 
-    // use iteration
+    /** Get the item at the specified index
+     * Must use iteration, not recursion
+     */
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index > size) {
             return null;
         }
-        Node<T> pointer = sentinel;
-        for(int i = 0; i <= index; i++) {
-            pointer = pointer.next;
+
+        Node<T> currentNode = sentinel;
+        for (int i = 0; i <= index; i++) {
+            currentNode = currentNode.next;
         }
-        return pointer.item;
+        return currentNode.data;
     }
 
+    /* Same as get(), but use recursion */
     public T getRecursive(int index, Node<T> node) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index > size) {
             return null;
         }
-        if (index == 0) {
-            return node.next.item;
-        } else {
-            return getRecursive(index - 1, node.next);
+        if (index == 1) {
+            return node.data;
+        }
+        return getRecursive(index - 1, node.next);
+    }
+
+    /** Build the iterator class for linked list deque
+     * Class method: hasNext(), next()
+     */
+    private class LinkedListDequeIterator implements Iterator<T> {
+        private int position;
+        private Node<T> currentNode;
+
+        public LinkedListDequeIterator() {
+            position = 0;
+            currentNode = sentinel;
+        }
+
+        public boolean hasNext() {
+            return position < size;
+        }
+
+        public T next() {
+            position += 1;
+            currentNode = currentNode.next;
+            return currentNode.data;
         }
     }
 
     public Iterator<T> iterator() {
-        return new LinkedListIterator();
+        return new LinkedListDequeIterator();  // Why need a new keyword?
     }
 
-    private class LinkedListIterator implements Iterator<T> {
-        private int pos;
-        private Node<T> currNode;
-
-        public LinkedListIterator() {
-            pos = 0;
-            currNode = sentinel;
-        }
-
-        public boolean hasNext() {
-            return pos < size;
-        }
-
-        public T next() {
-            pos += 1;
-            currNode = currNode.next;
-            return currNode.item;
-        }
-    }
-
+    /* Comparison using class method */
     @Override
     public boolean equals(Object o) {
         if (o == null) {
@@ -150,19 +169,21 @@ public class LinkedListDeque<T> implements Deque<T> {
             return true;
         }
 
-        LinkedListDeque<T> other = (LinkedListDeque<T>) o;
-        if(other.size() != this.size()) {
+        // Casting
+        LinkedListDeque<T> otherObject = (LinkedListDeque<T>) o;
+        if (otherObject.size() != size()) {
             return false;
         }
 
-        Iterator<T> otherIterator = other.iterator();
-        Iterator<T> thisIterator = this.iterator();
-
-        while(otherIterator.hasNext() && thisIterator.hasNext()) {
-            if(thisIterator.next() != otherIterator.next()) {
+        Iterator<T> iteratorOther = otherObject.iterator();
+        Iterator<T> iteratorThis = iterator();
+        while (iteratorOther.hasNext() && iteratorThis.hasNext()) {
+            if (iteratorOther.next() != iteratorThis.next()) {
                 return false;
             }
         }
+
+        // All tests passed, return true
         return true;
     }
 }
